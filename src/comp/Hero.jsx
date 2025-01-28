62e35959b0baed271623f67d081a991766167d97
Asher <carneiroasher@gmail.com>
2025-01-29 01:15:12 +0530

    basic positioning works

--- src/comp/Hero.jsx
+++ src/comp/Hero.jsx
@@ -14,9 +14,20 @@ function Hero() {
     
       return (
         <>
-          <div className="Hero">
+          <div className="hero-container">
             <div className="bg" id="vanta">
               
+            <div className="content">
+                <h1 className="title">IETE Committee</h1>
+                <div className="logo-container">
+                    <img src="src/assets/iete-rectangle.jpg" alt="IETE SFIT" className="logo" />
+                </div>
+                <p className="tagline">&gt; Empowering Tech of Today.</p>
+                <button className="contact-button">
+                    Contact Us <span className="chat-icon">💬</span>
+                </button>
+            </div>
+
             </div>
           </div>
         </>
