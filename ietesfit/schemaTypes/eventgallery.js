1c300e89474092a58c78e57a820d3af58743249f
VaibhavSutar <sutarvaibhav37@gmail.com>
2025-03-12 11:15:24 -0600

    sanity 2

--- ietesfit/schemaTypes/eventgallery.js
+++ ietesfit/schemaTypes/eventgallery.js
@@ -1,29 +1,35 @@
 export default {
-    name: "eventGallery",
-    title: "Event Gallery",
-    type: "document",
-    fields: [
-      {
-        name: "title",
-        title: "Event Title",
-        type: "string",
-      },
-      {
-        name: "date",
-        title: "Event Date",
-        type: "string",
-      },
-      {
-        name: "images",
-        title: "Event Images",
-        type: "array",
-        of: [{ type: "image" }],
-      },
-      {
-        name: "redirectUrl",
-        title: "Redirect URL",
-        type: "url",
-      },
-    ],
-  };
-  
+  name: "eventGallery",
+  title: "Event Gallery",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Event Title",
+      type: "string",
+    },
+    {
+      name: "images",
+      title: "Event Images",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: "eventName",
+              title: "Event Name",
+              type: "string",
+            },
+            {
+              name: "asset",
+              title: "Image",
+              type: "image",
+              options: { hotspot: true },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
