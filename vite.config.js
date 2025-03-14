f00eef60a04d29988f4cf5430eb19d833845ebfb
VaibhavSutar <sutarvaibhav37@gmail.com>
2025-03-14 10:47:23 -0600

    deploy issue 4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // This matches the distDir in your vercel.json
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
-});
+  
+})
